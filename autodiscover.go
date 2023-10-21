dc957110e4153c32bfc6d2311c04aa19a1b61f75
Ed <edwin.ludik@gmail.com>
2023-10-21 09:47:49 +0200

    add .env and battery

--- autodiscover.go
+++ autodiscover.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"time"
+
+	"github.com/distatus/battery"
+	"github.com/joho/godotenv"
 
 	"github.com/jaedle/golang-tplink-hs100/pkg/configuration"
 	"github.com/jaedle/golang-tplink-hs100/pkg/hs100"
 )
 
 func main() {
-	devices, err := hs100.Discover("192.168.1.0/24",
+	// load .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// get battery info
+	batteries, err := battery.GetAll()
+	if err != nil {
+		fmt.Println("Could not get battery info!")
+		return
+	}
+	for i, battery := range batteries {
+		fmt.Printf("Bat%d: ", i)
+		fmt.Printf("state: %s, ", battery.State.String())
+		fmt.Printf("current capacity: %f mWh, ", battery.Current)
+		fmt.Printf("last full capacity: %f mWh, ", battery.Full)
+		fmt.Printf("design capacity: %f mWh, ", battery.Design)
+		fmt.Printf("charge rate: %f mW, ", battery.ChargeRate)
+		fmt.Printf("voltage: %f V, ", battery.Voltage)
+		fmt.Printf("design voltage: %f V\n", battery.DesignVoltage)
+	}
+
+	// get the available plugs
+	networkMask := os.Getenv("NETWORK_MASK")
+	devices, err := hs100.Discover(networkMask,
 		configuration.Default().WithTimeout(time.Second),
 	)
 
--- go.mod
+++ go.mod
@@ -4,4 +4,10 @@ go 1.21.3
 
 require github.com/jaedle/golang-tplink-hs100 v0.6.1
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/distatus/battery v0.11.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	howett.net/plist v1.0.0 // indirect
+)
